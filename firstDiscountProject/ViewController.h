a176cf8bb781f107b2917dcf161f736974c7c820
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-11-25 15:30:09 -0500

    Initial Commit

---
+++ firstDiscountProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstDiscountProject
+//
+//  Created by Joshua on 11/25/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
